7238f317f990074e782550ff0d59f4c07d179c86
Levin <165691023@qq.com>
2012-07-02 15:40:56 +0800

    labi

--- Labixiaoxin1-10/Constants.h
+++ Labixiaoxin1-10/Constants.h
@@ -16,10 +16,10 @@
 #define APP_RATING_URL          @"itms-apps://ax.itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=525300493"
 #define ADMOB_ID                @"a14fb644e629a23"
 
-#define USE_MOBWIN
+//#define USE_MOBWIN
 #define MobWIN_ID               @"A09199F505177789A65CCA9EA86259F8"
 
-//#define USE_YOUMI
+#define USE_YOUMI
 #define Youmi_Ad_Id             @"4d698d73b01a2201"
 #define Youmi_Ad_Secret         @"3519b578598f5c00"
 
